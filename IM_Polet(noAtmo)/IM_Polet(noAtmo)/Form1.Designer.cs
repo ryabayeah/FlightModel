2adcd45dcb3ecc5e3fa8c299714155d2cb1da2dd
Anastasia Abazovskaya <ryabayeah@gmail.com>
2021-02-23 23:58:31 +0700

    add atmosphere and some fixes

--- IM_Polet(noAtmo)/IM_Polet(noAtmo)/Form1.Designer.cs
+++ IM_Polet(noAtmo)/IM_Polet(noAtmo)/Form1.Designer.cs
@@ -35,6 +35,10 @@ namespace IM_Polet_noAtmo_
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartFlight = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nWeight = new System.Windows.Forms.NumericUpDown();
+            this.nSize = new System.Windows.Forms.NumericUpDown();
             this.lableTime = new System.Windows.Forms.Label();
             this.btnStopContinue = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
@@ -47,6 +51,8 @@ namespace IM_Polet_noAtmo_
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chartFlight)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nSpeed)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nAngle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nHeight)).BeginInit();
@@ -66,7 +72,7 @@ namespace IM_Polet_noAtmo_
             this.chartFlight.Cursor = System.Windows.Forms.Cursors.Default;
             legend1.Name = "Legend1";
             this.chartFlight.Legends.Add(legend1);
-            this.chartFlight.Location = new System.Drawing.Point(12, 173);
+            this.chartFlight.Location = new System.Drawing.Point(13, 173);
             this.chartFlight.Name = "chartFlight";
             this.chartFlight.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
             series1.BorderWidth = 3;
@@ -78,13 +84,17 @@ namespace IM_Polet_noAtmo_
             series1.MarkerStep = 10;
             series1.Name = "Flying Object";
             this.chartFlight.Series.Add(series1);
-            this.chartFlight.Size = new System.Drawing.Size(619, 385);
+            this.chartFlight.Size = new System.Drawing.Size(716, 385);
             this.chartFlight.TabIndex = 0;
             this.chartFlight.Text = "chart1";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.nWeight);
+            this.panel1.Controls.Add(this.nSize);
             this.panel1.Controls.Add(this.lableTime);
             this.panel1.Controls.Add(this.btnStopContinue);
             this.panel1.Controls.Add(this.btnSave);
@@ -96,8 +106,53 @@ namespace IM_Polet_noAtmo_
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(618, 154);
+            this.panel1.Size = new System.Drawing.Size(716, 154);
             this.panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(282, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Weight";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(282, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Size";
+            // 
+            // nWeight
+            // 
+            this.nWeight.Location = new System.Drawing.Point(367, 47);
+            this.nWeight.Name = "nWeight";
+            this.nWeight.Size = new System.Drawing.Size(120, 20);
+            this.nWeight.TabIndex = 10;
+            this.nWeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nSize
+            // 
+            this.nSize.DecimalPlaces = 2;
+            this.nSize.Location = new System.Drawing.Point(367, 10);
+            this.nSize.Name = "nSize";
+            this.nSize.Size = new System.Drawing.Size(120, 20);
+            this.nSize.TabIndex = 9;
+            this.nSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            131072});
             // 
             // lableTime
             // 
@@ -112,7 +167,7 @@ namespace IM_Polet_noAtmo_
             // btnStopContinue
             // 
             this.btnStopContinue.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnStopContinue.Location = new System.Drawing.Point(448, 47);
+            this.btnStopContinue.Location = new System.Drawing.Point(530, 10);
             this.btnStopContinue.Name = "btnStopContinue";
             this.btnStopContinue.Size = new System.Drawing.Size(142, 48);
             this.btnStopContinue.TabIndex = 7;
@@ -123,7 +178,7 @@ namespace IM_Polet_noAtmo_
             // btnSave
             // 
             this.btnSave.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnSave.Location = new System.Drawing.Point(243, 47);
+            this.btnSave.Location = new System.Drawing.Point(530, 71);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(142, 48);
             this.btnSave.TabIndex = 6;
@@ -194,6 +249,7 @@ namespace IM_Polet_noAtmo_
             // 
             // timer1
             // 
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
@@ -201,7 +257,7 @@ namespace IM_Polet_noAtmo_
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(654, 570);
+            this.ClientSize = new System.Drawing.Size(738, 570);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.chartFlight);
             this.Name = "Form1";
@@ -209,6 +265,8 @@ namespace IM_Polet_noAtmo_
             ((System.ComponentModel.ISupportInitialize)(this.chartFlight)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nSpeed)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nAngle)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nHeight)).EndInit();
@@ -230,6 +288,10 @@ namespace IM_Polet_noAtmo_
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnStopContinue;
         private System.Windows.Forms.Label lableTime;
+        private System.Windows.Forms.NumericUpDown nSize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nWeight;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- IM_Polet(noAtmo)/IM_Polet(noAtmo)/Form1.cs
+++ IM_Polet(noAtmo)/IM_Polet(noAtmo)/Form1.cs
@@ -22,29 +22,32 @@ namespace IM_Polet_noAtmo_
         }
         private void btnSave_Click(object sender, EventArgs e)
         {
-            var tuple  = Utils.CheckFields((double)nSpeed.Value, (double)nAngle.Value);
-            lableTime.Text = tuple.output;
-            if (tuple.pass)
+            if(!timer1.Enabled)
             {
-                flight = new Flight((double)nHeight.Value, (double)nSpeed.Value, (double)nAngle.Value);
+                flight = new Flight(
+                        (double)nHeight.Value,
+                        (double)nSpeed.Value, 
+                        (double)nAngle.Value, 
+                        (double)nSize.Value, 
+                        (double)nWeight.Value
+                        );
+
                 btnStopContinue.Text = "Stop";
                 chartFlight.Series[0].Points.Clear();
                 chartFlight.Series[0].Points.AddXY(flight.x, flight.y);
 
-                var tulpe = flight.CalculateMaxCoordinates();
-                chartFlight.ChartAreas[0].AxisX.Maximum = tulpe.X;
-                chartFlight.ChartAreas[0].AxisY.Maximum = tulpe.Y;
+                chartFlight.ChartAreas[0].AxisX.Maximum = flight.CalculateMaxDistance();
+                chartFlight.ChartAreas[0].AxisY.Maximum = flight.CalculateMaxHeight();
 
                 timer1.Start();
                 btnStopContinue.Enabled = true;
                 btnStopContinue.BackColor = SystemColors.ButtonFace;
             }
-               
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            lableTime.Text = "Time: " + flight.timer++;
+            lableTime.Text = "Time: " + flight.timer++ + " sec";
             flight.CalculateCoordinates();
             chartFlight.Series[0].Points.AddXY(flight.x, flight.y);
             if (flight.y <= 0) timer1.Stop();
@@ -67,75 +70,48 @@ namespace IM_Polet_noAtmo_
         }
     }
 
-    public static class Utils
-    {
-        public static (bool pass,string output) CheckFields(double speed, double angle)
-        {
-            string output = "";
-            (bool pass, string output) t2 = (true, output);
-            Dictionary<string, double> Fields = new Dictionary<string, double>
-                {
-                    {"Speed", speed},
-                    {"Angle", angle}
-                };
-            foreach (var i in Fields)
-            {
-                if (i.Value <= 0)
-                {
-                     output = String.Format("Enter value of {0} greater than 0", i.Key);
-                    t2 = (false, output);
-                    return t2;
-                    
-                }
-            }
-            return t2;
-
-        }
-    }
     public class Flight
     {
-        const double dt = 0.01;
-        const double g = 9.81;
-        public double x;
-        public double y;
-        public double t;
-        public double a;
-        public double y0;
-        public double v0;
-        public double xMax;
-        public double yMax;
+        const double dt = 0.01, g = 9.81, c = 0.15, rho = 1.29;
+        public double x, y, t, a, alpha;
+        public double s, m, k;
+        public double y0, v0;
+        public double vx, vy, v;
         public bool state = true;
         public int timer = 0;
-
-
-        public Flight(double height, double speed, double angle)
+        public Flight(double height, double speed, double angle,double size,double weight)
         {
             y0 = height;
             v0 = speed;
-            a = angle;
-            state = true;
-            timer = 0;
             x = 0;
             y = y0;
             t = 0;
+            a = angle;
+            alpha = a * Math.PI / 180;
+            s = size;
+            m = weight;
+            k = 0.5 * c * rho * s / m;
+            vx = v0 * Math.Sin(a * Math.PI / 180);
+            vy = v0 * Math.Cos(a * Math.PI / 180);
+            state = true;
+            timer = 0;
+            
         }
 
         public void CalculateCoordinates()
         {
+            v = (double)Math.Sqrt((double)(vx * vx + vy * vy));
             t += dt;
-            x = v0 * Math.Cos(a * Math.PI / 180) * t;
-            y = y0 + v0 * Math.Sin(a * Math.PI / 180) * t - g * t * t / 2;
+            vx = vx - k * vx * v * dt;
+            vy = vy - (g + k * vy * v) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
+
         }
-        public  (double X, double Y) CalculateMaxCoordinates()
-        {
-            double tMax = (v0 * Math.Sin(a * Math.PI / 180) + Math.Sqrt(Math.Pow(v0 * Math.Sin(a * Math.PI / 180), 2) + 2 * g * y0)) / g;
-            double yMax = y0 + Math.Pow(v0 * Math.Sin(a * Math.PI / 180), 2) / (2 * g);
-            double xMax = v0 * Math.Cos(a * Math.PI / 180) * tMax;
-            double AxisX = Math.Round(xMax, 2) + Math.Round(xMax * 0.2, 3);
-            double AxisY = Math.Round(yMax, 2) + Math.Round(yMax * 0.2, 3);
-            (double X, double Y)t2 = (AxisX, AxisY);
-            return t2;
-        }
+        private double CalculateMaxTime() { return (v0 * Math.Sin(alpha) + Math.Sqrt(Math.Pow(v0 * Math.Sin(alpha), 2) + 2 * g * y0)) / g; }
+        public double CalculateMaxHeight() { return Math.Round(y0 + v0 * v0 * Math.Pow(Math.Sin(alpha), 2) / (2 * g), 2) + 0.2; }
+        public double CalculateMaxDistance() { return Math.Round(CalculateMaxTime() * v0 * Math.Cos(alpha), 2) + 0.2; }
+
     }
 }
 
